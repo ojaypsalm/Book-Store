c16b048b4f6081c5c40ae5a85a977fbe77ea68a7
ojo samuel <adekanmisamuel4@gmail.com>
2024-06-30 18:50:37 +0100

    some adjustment was made  to the code!

--- backend/utils/Cloudinary.js
+++ backend/utils/Cloudinary.js
@@ -4,8 +4,7 @@ const cloudinary = require('cloudinary').v2
 cloudinary.config({
     cloud_name: 'CLOUDINARY_CLOUD_NAME',
     api_key: 'CLOUDINARY_API_KEY',
-    api_secret: 'BHrazBwK5uIW6Qvf0LbNk7HrWIE' 
-    //api environment variable: 'CLOUDINARY_URL=cloudinary://615426354229647:BHrazBwK5uIW6Qvf0LbNk7HrWIE@divsjisaw'
+    api_secret: 'CLOUDINARY_API_SECRET' 
 })
 
 module.exports = cloudinary;
