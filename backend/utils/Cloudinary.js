29b33ab6c0c901fc5a76736749089c9707a9df50
ojo samuel <adekanmisamuel4@gmail.com>
2024-06-30 18:42:50 +0100

    authentication and additional security was added to the code!

--- backend/utils/Cloudinary.js
+++ backend/utils/Cloudinary.js
@@ -2,8 +2,8 @@ const cloudinary = require('cloudinary').v2
 
 
 cloudinary.config({
-    cloud_name: 'divsjisaw',
-    api_key: '615426354229647',
+    cloud_name: 'CLOUDINARY_CLOUD_NAME',
+    api_key: 'CLOUDINARY_API_KEY',
     api_secret: 'BHrazBwK5uIW6Qvf0LbNk7HrWIE' 
     //api environment variable: 'CLOUDINARY_URL=cloudinary://615426354229647:BHrazBwK5uIW6Qvf0LbNk7HrWIE@divsjisaw'
 })
